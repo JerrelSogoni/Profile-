9842cd532c5f256812014cf5cf67d5e09670a8ec
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2016-10-28 03:26:51 -0400

    Added scripts to git for user creation

---
+++ src/CreateDatascripts/UserDataGenerator.py
@@ -0,0 +1,28 @@
+
+#Made by Jerrel Sogoni, Creates Userdata in our database
+with open('MockUserData.txt') as f:
+    lines = f.read().splitlines()
+count = 0
+f = open('mockUsers.sql','w')
+for line in lines:
+	if(count == 0):
+		f.write( "INSERT INTO UserPlus VALUES(" + line + ", ")
+		count += 1
+	elif(count == 6):
+		f.write( line + ", ")
+		count += 1
+	elif(count == 9):
+		f.write( line + ", ")
+		count += 1
+	elif(count == 10):
+		f.write(line + ", ")
+		count += 1
+	elif(count == 12):
+		f.write( "'" + line + "'" + "\n" + ");\n")
+		count = 0
+	else:
+		f.write( "'" + line + "'" + ", ")
+		count += 1
+
+
+
