c6b8cc589ff68584d06ee1ca901ac8559f78e431
Yun Joon Soh <yunjoon.soh@gmail.com>
2016-10-31 16:13:02 -0400

    Separated into different folders, modifed py file to format the output

--- src/CreateDatascripts/UserDataGenerator.py
+++ src/CreateDatascripts/UserDataGenerator.py
@@ -1,10 +1,11 @@
 
 #Made by Jerrel Sogoni, Creates Userdata in our database
-with open('MockUserData.txt') as f:
+with open('./rsrc/MockUserData.txt') as f:
     lines = f.read().splitlines()
 count = 0
 f = open('mockUsers.sql','w')
 for line in lines:
+	line.strip()
 	if(count == 0):
 		f.write( "INSERT INTO UserPlus VALUES(" + line + ", ")
 		count += 1
@@ -18,7 +19,7 @@ for line in lines:
 		f.write(line + ", ")
 		count += 1
 	elif(count == 12):
-		f.write( "'" + line + "'" + "\n" + ");\n")
+		f.write( "'" + line + "'" + ");\n")
 		count = 0
 	else:
 		f.write( "'" + line + "'" + ", ")
