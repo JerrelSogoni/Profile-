e69c166eb1a0d5beccf7b767387735ebcdcdbc92
jthomas1294 <jane.thomas@stonybrook.edu>
2016-11-05 02:12:16 -0400

    Add files via upload

---
+++ src/MockDataSql/GroupPlusMock.sql
@@ -0,0 +1,5 @@
+insert into GroupPlus (GroupID, Owner, GroupName) values (26, 1, 'Automated value-added process improvement');
+insert into GroupPlus (GroupID, Owner, GroupName) values (27, 2, 'Diverse next generation knowledge base');
+insert into GroupPlus (GroupID, Owner, GroupName) values (28, 3, 'Inverse contextually-based forecast');
+insert into GroupPlus (GroupID, Owner, GroupName) values (29, 4, 'Team-oriented contextually-based solution');
+insert into GroupPlus (GroupID, Owner, GroupName) values (30, 5, 'Enterprise-wide encompassing portal');
