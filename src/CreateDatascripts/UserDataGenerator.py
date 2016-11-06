4fb369feb5fb8e2b4930e72f382e03efe4b6d3f5
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2016-11-06 15:03:46 -0500

    users have passwords

--- src/CreateDatascripts/UserDataGenerator.py
+++ src/CreateDatascripts/UserDataGenerator.py
@@ -1,7 +1,7 @@
 
 #Made by Jerrel Sogoni, Creates Userdata in our database
-with open('./rsrc/MockUserData.txt') as f:
-    lines = f.read().splitlines()
+with open('../rsrc/MockUserData.txt') as f:
+	lines = f.read().splitlines()
 count = 0
 f = open('mockUsers.sql','w')
 for line in lines:
@@ -9,16 +9,19 @@ for line in lines:
 	if(count == 0):
 		f.write( "INSERT INTO UserPlus VALUES(" + line + ", ")
 		count += 1
-	elif(count == 6):
-		f.write( line + ", ")
-		count += 1
-	elif(count == 9):
+	elif(count ==1):
+		f.write( "'" + line + "', ")
+		count +=1
+	elif(count == 7):
 		f.write( line + ", ")
 		count += 1
 	elif(count == 10):
+		f.write( line + ", ")
+		count += 1
+	elif(count == 11):
 		f.write(line + ", ")
 		count += 1
-	elif(count == 12):
+	elif(count == 13):
 		f.write( "'" + line + "'" + ");\n")
 		count = 0
 	else:
