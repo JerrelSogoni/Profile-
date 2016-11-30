67ef67d6af132693a6eb71e771628e144268d065
Yun Joon Soh <yunjoon.soh@gmail.com>
2016-11-30 06:06:56 -0500

    Register works with DB

---
+++ phase3/Backend_Netbeans_Project/src/java/profile/Register.java
@@ -0,0 +1,220 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profile;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import profile.util.DataConnect;
+import profile.util.JsfUtil;
+
+/**
+ *
+ * @author yunjoon_soh
+ */
+@Named("register")
+@ApplicationScoped
+public class Register implements Serializable{
+    private String phone;
+    private String firstname, lastname, email, address, city, state, zip,
+            dob, pref, sex;
+
+    /**
+     * @return the firstname
+     */
+    public String getFirstname() {
+        return firstname;
+    }
+
+    /**
+     * @param firstname the firstname to set
+     */
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    /**
+     * @return the lastname
+     */
+    public String getLastname() {
+        return lastname;
+    }
+
+    /**
+     * @param lastname the lastname to set
+     */
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @return the state
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * @param state the state to set
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     * @return the zip
+     */
+    public String getZip() {
+        return zip;
+    }
+
+    /**
+     * @param zip the zip to set
+     */
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    /**
+     * @return the dob
+     */
+    public String getDob() {
+        return dob;
+    }
+
+    /**
+     * @param dob the dob to set
+     */
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    /**
+     * @return the pref
+     */
+    public String getPref() {
+        return pref;
+    }
+
+    /**
+     * @param pref the pref to set
+     */
+    public void setPref(String pref) {
+        this.pref = pref;
+    }
+
+    /**
+     * @return the sex
+     */
+    public String getSex() {
+        return sex;
+    }
+
+    /**
+     * @param sex the sex to set
+     */
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+    
+    public String submit(){
+        Connection con = null;
+        PreparedStatement ps = null;
+
+        try {
+            con = DataConnect.getConnection();
+            ps = con.prepareStatement("INSERT INTO UserPlus("
+                    + "FirstName, LastName, Address, City, State, ZipCode, "
+                    + "Phone, Email) "
+                    + "VALUES(?, ?, ?, ?, ?, ?, ?, ?);");
+            ps.setString(1, firstname);
+            ps.setString(2, lastname);
+            ps.setString(3, address);
+            ps.setString(4, city);
+            ps.setString(5, state);
+            ps.setString(6, zip);
+            ps.setString(7, phone);
+            ps.setString(8, email);
+            
+            
+            // print out the query statement
+            JsfUtil.addErrorMessage(ps.toString());
+
+            int rs = ps.executeUpdate();
+            System.out.println("Updated result:" + rs);
+
+        } catch (SQLException ex) {
+            
+            // print out error message
+            JsfUtil.addErrorMessage("Connection to database failed:" + ex.getMessage());
+            System.out.println("Login error -->" + ex.getMessage());
+            return "/userPlus/Register";
+        } finally {
+            DataConnect.close(con);
+        }
+        return "";
+    }
+    
+}
--- phase3/Backend_Netbeans_Project/src/java/profile/UserPlusController.java
+++ phase3/Backend_Netbeans_Project/src/java/profile/UserPlusController.java
@@ -8,7 +8,6 @@ import java.util.ResourceBundle;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
