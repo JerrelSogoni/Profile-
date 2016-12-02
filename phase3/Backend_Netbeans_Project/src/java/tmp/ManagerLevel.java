ea064de5d17e2cbe3441bd71607c7463fbc79726
Yun Joon Soh <yunjoon.soh@gmail.com>
2016-12-02 00:02:20 -0500

    AdsList

--- phase3/Backend_Netbeans_Project/src/java/tmp/ManagerLevel.java
+++ phase3/Backend_Netbeans_Project/src/java/tmp/ManagerLevel.java
@@ -39,7 +39,7 @@ public class ManagerLevel implements Serializable {
     }
 
     public String listAds() {
-        return null;
+        return "/ManagerLevel/AdsList";
     }
 
     public String listTrans() {
