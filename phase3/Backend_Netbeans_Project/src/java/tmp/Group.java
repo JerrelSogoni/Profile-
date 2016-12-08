e632e078f618146a3fa1623055684157af2bd60e
Yun Joon Soh <yunjoon.soh@gmail.com>
2016-12-08 15:32:43 -0500

    Group Started

--- phase3/Backend_Netbeans_Project/src/java/tmp/Group.java
+++ phase3/Backend_Netbeans_Project/src/java/tmp/Group.java
@@ -1,7 +1,18 @@
 package tmp;
 
-
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import profile.GroupPlus;
+import profile.UserPlus;
+import profile.util.DataConnect;
+import profile.util.JsfUtil;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -13,74 +24,287 @@ import javax.inject.Named;
  * @author yunjoon_soh
  */
 @Named("group")
-public class Group {
-
+@SessionScoped
+public class Group implements Serializable {
+    
     public String createGrp() {
-        return null;
+        return "/groupPlus/Create";
     }
-
+    
     public String deleteGrp() {
-        return null;
+        return "/groupPlus/List";
     }
-
+    
     public String renameGrp() {
-        return null;
+        return "/groupPlus/List";
     }
-
-    public String addToGrp() {
-        return null;
+    
+    public String addToGrp() {        
+        return "/GroupLevel/GroupList";
     }
-
+    
     public String rmFromGroup() {
         return null;
     }
-
+    
     public String join() {
         return null;
     }
-
+    
     public String unjoin() {
         return null;
     }
-
+    
     public String makePostGrp() {
         return null;
     }
-
+    
     public String commentPostGrp() {
         return null;
     }
-
+    
     public String removePostGrp() {
         return null;
     }
-
+    
     public String removeCommentGrp() {
         return null;
     }
-
+    
     public String likePostGrp() {
         return null;
     }
-
+    
     public String likeCommentGrp() {
         return null;
     }
-
+    
     public String unlikePostGrp() {
         return null;
     }
-
+    
     public String unlikeCommentGrp() {
         return null;
     }
-
+    
     public String editPostGrp() {
         return null;
     }
-
+    
     public String editCommentGrp() {
         return null;
     }
+    
+    private UserPlus queryUserPlus;
+    private GroupPlus queryGroupPlus;
+    
+    public UserPlus getQueryUserPlus() {
+        return queryUserPlus;
+    }
+    
+    public void setQueryUserPlus(UserPlus queryUserPlus) {
+        this.queryUserPlus = queryUserPlus;
+    }
+    
+    public GroupPlus getQueryGroupPlus() {
+        return queryGroupPlus;
+    }
+    
+    public void setQueryGroupPlus(GroupPlus queryGroupPlus) {
+        this.queryGroupPlus = queryGroupPlus;
+    }
+    
+    private List<UserGroupPair> userInGroupList, groupWithUsersList;
+    
+    public class UserGroupPair {
+        
+        private String userId, userName, groupId, groupName;
+        
+        public String getUserId() {
+            return userId;
+        }
+        
+        public void setUserId(String userId) {
+            this.userId = userId;
+        }
+        
+        public String getUserName() {
+            return userName;
+        }
+        
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+        
+        public String getGroupId() {
+            return groupId;
+        }
+        
+        public void setGroupId(String groupId) {
+            this.groupId = groupId;
+        }
+        
+        public String getGroupName() {
+            return groupName;
+        }
+        
+        public void setGroupName(String groupName) {
+            this.groupName = groupName;
+        }
+        
+    }
+    
+    public List<UserGroupPair> getUserInGroupList() {
+        userInGroupList = new ArrayList<>();
+        
+        Connection con = null;
+        PreparedStatement ps = null;
+        
+        try {
+            con = DataConnect.getConnection();
+            ps = con.prepareStatement("SELECT * FROM IsIn I WHERE I.UserId = ?;");
+            
+            if (queryUserPlus != null) {
+                ps.setInt(1, queryUserPlus.getUserId());
+            }
 
+            // print out the query statement
+            JsfUtil.addErrorMessage(ps.toString());
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                //result found, means valid inputs
+                UserGroupPair added = new UserGroupPair();
+                
+                added.setGroupId(rs.getString("GroupId"));
+                added.setUserId(rs.getString("UserId"));
+                
+                userInGroupList.add(added);
+            }
+            
+        } catch (SQLException ex) {
+
+            // print out error message
+            JsfUtil.addErrorMessage("Connection to database failed:" + ex.getMessage());
+            System.out.println("Login error -->" + ex.getMessage());
+            
+        } finally {
+            DataConnect.close(con);
+        }
+        
+        return userInGroupList;
+    }
+    
+    public void setUserInGroupList(List<UserGroupPair> userInGroupList) {
+        this.userInGroupList = userInGroupList;
+    }
+    
+    public List<UserGroupPair> getGroupWithUsersList() {
+        groupWithUsersList = new ArrayList<>();
+        
+        Connection con = null;
+        PreparedStatement ps = null;
+        
+        try {
+            con = DataConnect.getConnection();
+            ps = con.prepareStatement("SELECT * FROM IsIn I WHERE I.GroupId = ?;");
+            
+            if (queryGroupPlus != null) {
+                ps.setInt(1, queryGroupPlus.getGroupId());
+            }
+
+            // print out the query statement
+            JsfUtil.addErrorMessage(ps.toString());
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                //result found, means valid inputs
+                UserGroupPair added = new UserGroupPair();
+                
+                added.setGroupId(rs.getString("GroupId"));
+                added.setUserId(rs.getString("UserId"));
+                
+                groupWithUsersList.add(added);
+            }
+            
+        } catch (SQLException ex) {
+
+            // print out error message
+            JsfUtil.addErrorMessage("Connection to database failed:" + ex.getMessage());
+            System.out.println("Login error -->" + ex.getMessage());
+            
+        } finally {
+            DataConnect.close(con);
+        }
+        
+        return groupWithUsersList;
+    }
+    
+    public void setGroupWithUsersList(List<UserGroupPair> groupWithUsersList) {
+        this.groupWithUsersList = groupWithUsersList;
+    }
+    
+    public String addToGrpButton() {
+        if (queryUserPlus != null && queryGroupPlus != null) {
+            Connection con = null;
+            PreparedStatement ps = null;
+            
+            try {
+                con = DataConnect.getConnection();
+                ps = con.prepareStatement("INSERT INTO IsIn VALUES(?, ?);");
+                
+                ps.setInt(1, queryUserPlus.getUserId());
+                ps.setInt(2, queryGroupPlus.getGroupId());
+
+                // print out the query statement
+                JsfUtil.addErrorMessage(ps.toString());
+                
+                ps.execute();
+                
+            } catch (SQLException ex) {
+
+                // print out error message
+                JsfUtil.addErrorMessage("Connection to database failed:" + ex.getMessage());
+                System.out.println("Login error -->" + ex.getMessage());
+                
+            } finally {
+                DataConnect.close(con);
+            }
+        }
+        
+        return "/GroupLevel/GroupList";
+    }
+    
+    public String rmFromGrpButton(){
+        if (queryUserPlus != null && queryGroupPlus != null) {
+            Connection con = null;
+            PreparedStatement ps = null;
+            
+            try {
+                con = DataConnect.getConnection();
+                ps = con.prepareStatement("DELETE FROM IsIn WHERE UserId = ? AND GroupId = ?;");
+                
+                ps.setInt(1, queryUserPlus.getUserId());
+                ps.setInt(2, queryGroupPlus.getGroupId());
+
+                // print out the query statement
+                JsfUtil.addErrorMessage(ps.toString());
+                
+                ps.execute();
+                
+            } catch (SQLException ex) {
+
+                // print out error message
+                JsfUtil.addErrorMessage("Connection to database failed:" + ex.getMessage());
+                System.out.println("Login error -->" + ex.getMessage());
+                
+            } finally {
+                DataConnect.close(con);
+            }
+        }
+        
+        return "/GroupLevel/GroupList";
+    }
 }
